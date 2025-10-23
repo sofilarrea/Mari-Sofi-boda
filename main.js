cf8cc699edae77717538540a22841aec30c6f058
sofilarrea <sofialejandra.larrea@gmail.com>
2025-10-22 22:55:07 -0300

    a ver

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-
-  const hamburger = document.getElementById("hamburger");
-  const sidebar = document.getElementById("sidebar");
-
-  hamburger.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
   });
+});
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
 
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+});
